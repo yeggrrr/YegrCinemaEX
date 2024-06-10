0b5d28bce1b3f470800c721ca1409e90fb8f5c26
yeggrrr <yjkim981103@gmail.com>
2024-06-10 15:32:15 +0900

    Initial Commit

---
+++ YegrCinemaEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YegrCinemaEX
+//
+//  Created by YJ on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
